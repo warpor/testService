8abffbd82ba63ea37491d91bdb836628242038a7
warpor <sp_8@list.ru>
2023-10-14 21:30:43 +0500

    Add registration page

--- tests_service/apps/testing/urls.py
+++ tests_service/apps/testing/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 app_name = "testing"
 urlpatterns = [
+    path("registration/", views.sign_up, name="registration"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("welcome_page", views.WelcomeView.as_view(), name="welcome")
 ]
--- tests_service/apps/testing/views.py
+++ tests_service/apps/testing/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 
 class WelcomeView(LoginRequiredMixin, TemplateView):
     template_name = "testing/welcome_page.html"
+
+
+def sign_up(request: HttpRequest):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("testing:welcome")
+        return render(request, 'registration/register.html',
+                      {"form": form})
+    return render(request, 'registration/register.html',
+                  {"form": UserCreationForm()})
 # Create your views here.
