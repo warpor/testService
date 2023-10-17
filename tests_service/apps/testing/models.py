40bb4e0740d1277edad167cee43e299f138419e6
warpor <sp_8@list.ru>
2023-10-17 08:36:57 +0500

    Add model Choice

--- tests_service/apps/testing/models.py
+++ tests_service/apps/testing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Test(models.Model):
     title = models.CharField(max_length=200, unique=True, blank=False)
     active = models.BooleanField()
@@ -10,4 +11,11 @@ class Question(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
     active = models.BooleanField()
     question_text = models.TextField()
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.TextField()
+    right_answer = models.BooleanField()
+
 # Create your models here.
