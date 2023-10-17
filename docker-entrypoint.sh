7a8f8aec7e33fa6df01111d5c1203a2b252d6869
warpor <sp_8@list.ru>
2023-10-17 11:08:00 +0500

    Add check default superuser

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,9 @@ echo $DB_HOST
 python /code/tests_service/manage.py migrate
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('${ADMIN_NAME}',
- 'admin@example.com', '${ADMIN_PASSWORD}')" | python /code/tests_service/manage.py shell
+'admin@example.com', '${ADMIN_PASSWORD}') if len(User.objects.filter(username='${ADMIN_NAME}'))==0
+else print('user exist')" | python /code/tests_service/manage.py shell
+
+
 
 python /code/tests_service/manage.py runserver 0.0.0.0:8000
