ff4a30da70b1e4cf2bb4612340c1a69d670594c8
warpor <sp_8@list.ru>
2023-10-17 09:44:13 +0500

    Add function to skip question

--- tests_service/apps/testing/service_testing.py
+++ tests_service/apps/testing/service_testing.py
@@ -12,9 +12,47 @@ def session_initialization(test_id: int, request: HttpRequest):
     request.session["right_answers"] = 0
 
 
+def skip_question(request: HttpRequest):
+    first_element = request.session["questions_id"].pop(0)
+    request.session["questions_id"].append(first_element)  # Add first element in end of list
+
+
 def clear_session(request: HttpRequest):
     del request.session["questions_id"]
     del request.session["right_answers"]
+
+
+def finish_test(request: HttpRequest, test_id: int) -> HttpResponseRedirect:
+    test: Test = get_object_or_404(Test, pk=test_id)
+
+    save_result(request, test_id)
+    right_answer = request.session["right_answers"]
+    all_question_count = len(get_active_question(test))
+    context = get_info_about_attemps(right_answer, all_question_count)
+    clear_session(request)
+    return render(request, "testing/result.html", context)
+
+
+def check_answers(request: HttpRequest, question_id: int):
+    user_answers = request.POST.getlist("choice")
+    user_answers = set(map(int, user_answers))
+
+    right_answers \
+        = get_object_or_404(
+        Question, pk=question_id).choice_set.filter(right_answer=True)
+    right_answers = set(right_answers.values_list("id", flat=True))
+
+    if right_answers == user_answers:
+        request.session["right_answers"] += 1
+
+
+def get_new_page_with_question(request: HttpRequest, test_id: int):
+    question_id = request.session["questions_id"][0]
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "testing/question_answer.html",
+                  {"question": question, "test_id": test_id})
+
+
 def save_result(request: HttpRequest, test_id: int):
     test = get_object_or_404(Test, pk=test_id)
     user_attempt \
