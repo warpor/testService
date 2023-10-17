5d476a34bdf312c9efd65a2d148269dff954d8ab
warpor <sp_8@list.ru>
2023-10-17 09:38:52 +0500

    Add page with tests list

--- tests_service/apps/testing/urls.py
+++ tests_service/apps/testing/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("welcome_page", views.WelcomeView.as_view(), name="welcome")
     path("exit/", auth_views.LogoutView.as_view(), name="logout"),
+    path("welcome_page", views.WelcomeView.as_view(), name="welcome"),
 ]
--- tests_service/apps/testing/views.py
+++ tests_service/apps/testing/views.py
@@ -2,11 +2,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView, TemplateView
+
+from .models import Test, UserRightAnswer
+from .service_testing import session_initialization, skip_question, finish_test, check_answers, \
+    get_new_page_with_question, get_active_question, process_all_attempts
 
 
-class WelcomeView(LoginRequiredMixin, TemplateView):
+class WelcomeView(LoginRequiredMixin, ListView):
+    model = Test
     template_name = "testing/welcome_page.html"
+    context_object_name = "tests_list"
+
+    def get_queryset(self):
+        return self.model.objects.filter(active=True)
+
+
 
 
 def sign_up(request: HttpRequest):
