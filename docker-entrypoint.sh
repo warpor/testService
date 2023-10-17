e8919bc2f0c30c4642b94f11e56b21a040fa03f2
warpor <sp_8@list.ru>
2023-10-17 10:24:47 +0500

    Add default superuser

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # Collect static files
 # Apply database migrations
-echo "Apply database migrations"
 
+
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
+echo $DB_HOST
 python /code/tests_service/manage.py migrate
-#python tests_service/manage.py migrate testing 0002
-#python tests_service/manage.py migrate testing 0003
 
-python /code/tests_service/manage.py runserver
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('${ADMIN_NAME}',
+ 'admin@example.com', '${ADMIN_PASSWORD}')" | python /code/tests_service/manage.py shell
+
+python /code/tests_service/manage.py runserver 0.0.0.0:8000
