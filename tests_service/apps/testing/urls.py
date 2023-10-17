e0a9a738503b03842eaa2e06aae5ca8ef655402f
warpor <sp_8@list.ru>
2023-10-17 08:43:32 +0500

    Add logout view

--- tests_service/apps/testing/urls.py
+++ tests_service/apps/testing/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import path
 
 from . import views
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path("registration/", views.sign_up, name="registration"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("welcome_page", views.WelcomeView.as_view(), name="welcome")
+    path("exit/", auth_views.LogoutView.as_view(), name="logout"),
 ]
