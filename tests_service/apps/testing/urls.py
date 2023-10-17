d5d75c1d135c58bd9e4ef76de23f5f1cab82a5cb
warpor <sp_8@list.ru>
2023-10-17 11:36:03 +0500

    Bug fix

--- tests_service/apps/testing/urls.py
+++ tests_service/apps/testing/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("registration/", views.sign_up, name="registration"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("exit/", auth_views.LogoutView.as_view(), name="logout"),
-    path("/", views.WelcomeView.as_view()),
     path("welcome_page", views.WelcomeView.as_view(), name="welcome"),
     path("info/<int:pk>", views.InfoView.as_view(), name="info"),
     path("test/<int:test_id>", views.question_answer, name="attempt"),
