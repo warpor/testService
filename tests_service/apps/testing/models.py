425c8a3f5ace1fea535937a735a9e04fcc019228
warpor <sp_8@list.ru>
2023-10-17 08:34:39 +0500

    Add model Test

--- tests_service/apps/testing/models.py
+++ tests_service/apps/testing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Test(models.Model):
+    title = models.CharField(max_length=200, unique=True, blank=False)
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.title
 # Create your models here.
