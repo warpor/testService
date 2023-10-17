20bbfdcedf30730b3531d01393f73d93dc9970d3
warpor <sp_8@list.ru>
2023-10-17 08:40:20 +0500

    Add lib for nested inlines in admin

--- tests_service/config/settings.py
+++ tests_service/config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'nested_admin',
+
     'apps.testing.apps.TestingConfig'
 ]
 
@@ -124,7 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = os.path.join('/static/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
